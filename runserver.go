47e8096db3bbf272ac3b6c224f6806c0047f6d7f
itsara <khajer@yahoo.com>
2018-06-20 10:50:49 +0700

    change method name

--- runserver.go
+++ runserver.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, getTextHello())
+    fmt.Fprintf(w, getTextHelloWorldText())
 }
 
 func main() {
@@ -15,7 +15,7 @@ func main() {
 }
 
 //simple method 
-func getTextHello() string{
+func getTextHelloWorldText() string{
 	return "hello, world : microservice"
 }
 
