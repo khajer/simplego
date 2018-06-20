ca8aca98ced7d405bdad96d71942a932d05b1244
581722 <navanit_akra.sisuk@ktb.co.th>
2018-06-20 10:44:41 +0700

    modify method

--- runserver.go
+++ runserver.go
@@ -6,10 +6,16 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "hello, world, new microservice")
+    fmt.Fprintf(w, getTextHello())
 }
 
 func main() {
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
 }
+
+func getTextHello() string{
+	return "hello, world : microservice"
+}
+
+
